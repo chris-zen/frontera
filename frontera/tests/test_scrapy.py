2e4444e4c0833e338e8bc18a6f9d99e32a6167d5
Mark Baas <mark.baas@gmail.com>
2015-07-17 13:31:52 -0400

    fixes the not passing of scrapy request body
    fixed one issue in converter + tests

--- frontera/tests/test_scrapy.py
+++ frontera/tests/test_scrapy.py
@@ -20,7 +20,8 @@ def test_request_response_converters():
     rsc = ResponseConverter(spider, rc)
 
     url = "http://test.com/test?param=123"
-    request = ScrapyRequest(url=url, callback=spider.callback, errback=spider.errback)
+    request = ScrapyRequest(url=url, callback=spider.callback, errback=spider.errback,
+                            body="BodyContent")
     request.meta['test_param'] = 'test_value'
     request.headers.appendlist("TestKey", "test value")
     request.cookies['MyCookie'] = 'CookieContent'
@@ -28,6 +29,7 @@ def test_request_response_converters():
     frontier_request = rc.to_frontier(request)
     assert frontier_request.meta['scrapy_callback'] == 'callback'
     assert frontier_request.meta['scrapy_errback'] == 'errback'
+    assert frontier_request.meta['scrapy_body'] == 'BodyContent'
     assert frontier_request.url == url
     assert frontier_request.method == 'GET'
     assert frontier_request.headers['Testkey'] == 'test value'
@@ -36,6 +38,7 @@ def test_request_response_converters():
 
     request_converted = rc.from_frontier(frontier_request)
     assert request_converted.meta['test_param'] == 'test_value'
+    assert request_converted.body == 'BodyContent'
     assert request_converted.url == url
     assert request_converted.method == 'GET'
     assert request_converted.cookies['MyCookie'] == 'CookieContent'
@@ -44,14 +47,17 @@ def test_request_response_converters():
     # Some middleware could change .meta contents
     request_converted.meta['middleware_stuff'] = 'appeared'
 
-    response = ScrapyResponse(url=url, request=request_converted, headers={'TestHeader': 'Test value'})
+    response = ScrapyResponse(url=url, request=request_converted,
+                              headers={'TestHeader': 'Test value'})
     frontier_response = rsc.to_frontier(response)
+    assert frontier_response.meta['scrapy_body'] == 'BodyContent'
     assert frontier_response.meta['scrapy_meta']['test_param'] == 'test_value'
     assert frontier_response.meta['scrapy_meta']['middleware_stuff'] == 'appeared'
     assert frontier_response.status_code == 200
     assert 'frontier_request' not in frontier_response.meta['scrapy_meta']
 
     response_converted = rsc.from_frontier(frontier_response)
+    assert response_converted.body == 'BodyContent'
     assert response_converted.meta['test_param'] == 'test_value'
     assert response_converted.url == url
     assert response_converted.status == 200
